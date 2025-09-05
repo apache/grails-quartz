2d125842fec77e8606c20280060ba8af20d30534
James Daugherty <jdaugherty@jdresources.net>
2025-09-05 12:06:27 -0400

    chore: adjust verification script to not error when no differences remain

--- etc/bin/verify-reproducible.sh
+++ etc/bin/verify-reproducible.sh
@@ -141,6 +141,7 @@ if [ -s diff.txt ]; then
       fi
 
   done < diff.txt
+  : > diff_purged.txt  # Ensure the file exists and is empty
   mv diff_purged.txt diff.txt
   rm -rf firstArtifact secondArtifact firstSource secondSource || true
 
@@ -150,6 +151,7 @@ if [ -s diff.txt ]; then
   echo "❌ Differences Found ❌"
   else
     echo "✅ Differences were resolved via decompilation. ✅"
+    exit 0
   fi
 else
   echo "✅ No Differences Found. ✅"
